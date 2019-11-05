33050b7c87e1c8f83ed89ffd6ea63a9b2df2a33f
OscarRivas99 <00005218@uca.edu.sv>
2019-11-04 23:15:45 -0600

    Updates

---
+++ precache-manifest.7e8e1c2484686c99e45d92a7152a5dda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb095a8dc9cae7280783743d1d2af936",
+    "url": "/github_pages/index.html"
+  },
+  {
+    "revision": "f89b07e4ed776ce3c82f",
+    "url": "/github_pages/static/css/main.8201a99d.chunk.css"
+  },
+  {
+    "revision": "07a77a4069e10c4dd9e0",
+    "url": "/github_pages/static/js/2.a79e62db.chunk.js"
+  },
+  {
+    "revision": "f89b07e4ed776ce3c82f",
+    "url": "/github_pages/static/js/main.fbcae35c.chunk.js"
+  },
+  {
+    "revision": "476d7b4904e999a4b178",
+    "url": "/github_pages/static/js/runtime-main.5d99d55f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/github_pages/static/media/logo.25bf045c.svg"
+  }
+]);
